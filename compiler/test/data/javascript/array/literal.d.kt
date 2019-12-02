eabf31f203276e3bd41edb04e26052dc1be71366
unknown <seitz.m95@gmail.com>
2019-12-02 11:39:36 +0100

    Fix array test output

--- compiler/test/data/javascript/array/literal.d.kt
+++ compiler/test/data/javascript/array/literal.d.kt
@@ -19,4 +19,36 @@ external object arr {
     var `0`: String
     var `1`: String
     var `2`: String
+    var length: Number
+    fun concat(): Any?
+    fun copyWithin(target: Number, start: Number, end: Number): Number
+    fun fill(value: Any?, start: Number, end: Number): Any?
+    fun find(): Any?
+    fun findIndex(): Number
+    fun lastIndexOf(): Number
+    fun pop(): Any?
+    fun push(): Number
+    fun reverse(): Any?
+    fun shift(): Any?
+    fun unshift(): Any?
+    fun slice(): Any?
+    fun sort(): Any?
+    fun splice(): Any?
+    fun includes(element: Any?, start: Number): Boolean
+    fun indexOf(): Any?
+    fun join(): Any?
+    fun keys(): Any?
+    fun entries(): Any?
+    fun values(): Any?
+    fun forEach(): Any?
+    fun filter(): Any?
+    fun flat(): Any?
+    fun flatMap(): Any?
+    fun map(): Any?
+    fun every(): Any?
+    fun some(): Any?
+    fun reduce(): Any?
+    fun reduceRight(): Any?
+    fun toLocaleString(): String
+    fun toString(): String
 }
