38457beac6160ea8c4f7f1dfd05803224e0b82bb
Maximilian Seitz <seitz.m95@gmail.com>
2020-01-13 13:42:14 +0100

    Add 'RegExp' build-in class members

--- javascript/js-type-analysis/src/org/jetbrains/dukat/js/type/inbuilt/regExpClass.kt
+++ javascript/js-type-analysis/src/org/jetbrains/dukat/js/type/inbuilt/regExpClass.kt
@@ -3,4 +3,20 @@ package org.jetbrains.dukat.js.type.inbuilt
 
 import org.jetbrains.dukat.js.type.inbuilt.builder.jsClass
 
-val regExpClass by jsClass {  }
+val regExpClass by jsClass {
+    constructor("pattern" to string, "flags" to string)
+
+    "dotAll" isType boolean
+    "flags" isType string
+    "global" isType boolean
+    "ignoreCase" isType boolean
+    "multiline" isType boolean
+    "source" isType string
+    "sticky" isType boolean
+    "unicode" isType boolean
+    "lastIndex" isType number
+
+    "compile"("pattern" to string, "flags" to string) { void }
+    "exec"("str" to string) { any }
+    "test"("str" to string) { boolean }
+}
